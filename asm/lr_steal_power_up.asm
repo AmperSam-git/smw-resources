04939f09b1b68c1aa88d3c8f8839c5690ad35412
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-06-17 12:38:44 -0230

    Fix up l/r power-up stealing asm

--- asm/lr_steal_power_up.asm
+++ asm/lr_steal_power_up.asm
@@ -1,97 +1,110 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; Save Power-Up to Item Box with L/R by AmperSam
 ;
 ; This patch will allow you to "steal" any of the 4 valid powerup states that
-; Mario can have and place that corresponding power in the item box.
+; Mario can have and place that corresponding power in the item box. Since the star isn't
+; in the power-up list at $19, this ASM checks if the star timer is active instead.
 ;
-; Since the star isn't in the power up list at $19, this asm checks if the
-; star timer is active instead.
+; Note: if Mario is in a power-up state AND has star power, this ASM will steal the star power
+; first. Only pressing L/R after stealing the star will steal the power-up. Stealing star power
+; can be disabled.
 ;
-; Uses FreeRAM: 2 bytes
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 !give_iframes = 1                   ;> If 1, give player some invincibility when power stealing
 !iframes_time = $7F                 ;> number of frames to make player invincible (default: 7F)
 
+!steal_star = 1                     ;> Allow stealing star power
+
+!drop_existing_item = 1             ;> Drop the item already in the box when stealing
+
 !play_sfx = 1                       ;> If 1, play item box SFX when power stealing
 !sfx_value = $0B                    ;> SFX value
 !sfx_bank = $1DFC|!addr             ;> SFX bank
 
-!item_box = $0DC2|!addr             ;> address of item box
-
-!powerup_freeram = $1869|!addr      ;> freeram address to store old powerup state (1 byte)
-!star_freeram = $186A|!addr         ;> freeram address to star timer (1 byte)
-
 main:
+    lda $9D                         ;\ check if game is frozen or...
+    ora $13D4|!addr                 ;| if game is paused
+    bne .return                     ;/
+
     lda $19                         ;\ load powerup state and...
-    sta !powerup_freeram            ;/ store in FreeRAM
+    sta $00                         ;/ store in scratch ram
 
-    lda $1490|!addr                 ;\ load star timer and...
-    sta !star_freeram               ;/ store in FreeRAM
-
-    lda $18                         ;\ check if either L or R is pressed
-    bit #%00100000 : bne steal      ;| and run the steal power up routine
-    bit #%00010000 : bne steal      ;/
-    rtl
-
-steal:
-
-    lda $1490|!addr                 ;\ check if star or...
-    ora $19                         ;| powerup state is active
-    beq .return                     ;/
-
-    stz $19                         ;> remove player's power up
-
-    stz $1407|!addr                 ;> reset cape flight
-    stz $1490|!addr                 ;> remove star powerup
-    stz $13E0|!addr                 ;> reset player's pose
-
-    if !give_iframes
-        lda #!iframes_time          ;\ give i-frames
-        sta $1497|!addr             ;/
+    if !steal_star
+        lda $1490|!addr             ;\ load star timer and...
+        sta $01                     ;/ store in (different) scratch ram
     endif
 
-    if !play_sfx
-        lda #!sfx_value             ;\ play item get sfx for item box
-        sta !sfx_bank               ;/
-    endif
-
-    lda !powerup_freeram            ;\ compare stored powerup state with...
-    cmp #$01                        ;/ mushroom value
-    beq .store_mushroom
-
-    lda !powerup_freeram            ;\ compare stored powerup state with...
-    cmp #$02                        ;/ cape value
-    beq .store_cape
-
-    lda !powerup_freeram            ;\ compare stored powerup state with...
-    cmp #$03                        ;/ flower value
-    beq .store_flower
-
-    lda !star_freeram               ;\ check if star timer was active
-    cmp #$00                        ;/
-    bne .store_star
-
-    rtl
-
-.store_mushroom
-    lda #$01 : sta !item_box        ;> put mushroom in item box
-    rtl
-
-.store_flower
-    lda #$02 : sta !item_box        ;> put flower in item box
-    rtl
-
-.store_star
-    lda #$03 : sta !item_box        ;> put star in item box
-    rtl
-
-.store_cape
-    lda #$04 : sta !item_box        ;> put feather in item box
-    rtl
+    lda $18                         ;\ check if either L or R is pressed...
+    bit #%00110000 : bne steal      ;/ and run the steal routine
 
 .return
     rtl
+
+steal:
+    if !drop_existing_item
+        JSL $028008                 ;> drop existing item box item
+    endif
+
+    if !steal_star
+        lda $1490|!addr             ;\ check if star active...
+        ora $19                     ;| or powerup state
+        beq .return                 ;/
+    else
+        lda $19                     ;\ just check if powerup state is active
+        beq .return                 ;/
+    endif
+
+    if !give_iframes
+        lda #!iframes_time          ;\ take number of i-frames...
+        sta $1497|!addr             ;/ store in the invincibility timer
+    endif
+
+    if !play_sfx
+        lda #!sfx_value             ;\ play "item get" sfx for item box
+        sta !sfx_bank               ;/
+    endif
+
+    if !steal_star
+        ldx $01                     ;\ if a star was active...
+        bne .store_star             ;/ steal the star
+    endif
+
+    ldx $00                         ;\ if the player is small...
+    beq .return                     ;/ do nothing
+
+.store_powerup
+    dex
+    lda power_ups,x                 ;\ load the power up values...
+    sta $0DC2|!addr                 ;/ store to item box
+
+    stz $19                         ;> clear power up state
+    stz $1407|!addr                 ;\ reset flight state...
+    stz $13E0|!addr                 ;/ and player's pose (for cape)
+    bra .return
+
+.store_star
+    ldx #$03                        ;> load star value ($03 = star)
+    lda $01                         ;\ check if star timer was active...
+    beq .return                     ;/ if not do nothing
+
+    stx $0DC2|!addr                 ;> store star to item box
+
+    stz $18D3|!addr                 ;> clear star sparkles
+    stz $1490|!addr                 ;> remove star powerup
+
+..restore_music
+    lda $0DDA|!addr                 ;\ check the music register...
+    cmp #$FF                        ;| if #$FF (level end)...
+    beq .return                     ;/ do nothing
+    and #$7F                        ;\ clear bit 7 to stop star music...
+    sta $0DDA|!addr                 ;| store new value back in the register and...
+    sta $1DFB|!addr                 ;/ restore the song number
+
+.return
+    rtl
+
+power_ups:
+    db $01,$04,$02                  ;> $01 = mushroom, $04 = cape, $02 = flower
