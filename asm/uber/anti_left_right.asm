e3bfd173ae11904e28473f234ff6e3d1e57e3085
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-11-09 19:06:59 -0330

    Create anti_left_right.asm

---
+++ asm/uber/anti_left_right.asm
@@ -0,0 +1,30 @@
+; Prevent left-right by stopping Mario if both keys are held
+
+ !LeftInput = %00000010
+!RightInput = %00000001
+
+main:
+	LDA $9D
+	ORA $13D4|!addr
+	BNE .return
+
+.checkLeftInput
+	LDA $0DA2|!addr
+    AND #!LeftInput
+    BEQ .checkRighttInput
+
+.disableRightInput
+	LDA #!RightInput
+	TRB $15
+
+.checkRighttInput
+	LDA $0DA2|!addr
+    AND #!RightInput
+    BEQ .return
+
+.disableLeftInput
+	LDA #!LeftInput
+	TRB $15
+
+.return
+	rtl
