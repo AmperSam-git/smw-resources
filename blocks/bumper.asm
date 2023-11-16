90f253e45cd7756ba0106721527f2abc5473dc0b
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-11-15 21:42:55 -0330

    Update bumper.asm

--- blocks/bumper.asm
+++ blocks/bumper.asm
@@ -3,47 +3,61 @@ db $42
 JMP Return : JMP Return : JMP MarioSide : JMP Return : JMP SpriteH : JMP Return
 JMP Return : JMP Return : JMP Return : JMP Return
 
+!PlaySFX = 0 ; set to 1 to play a sound
+
+Speed:
+    db $E0,$20
+
+MarioSide:
+    LDX $93
+    LDA Speed,x
+    STA $7B
+    RTL
+
 SpriteH:
     ; Check if throw block
-	LDA !9E,x
-	CMP #$53
-	BNE Return
+    LDA !9E,x
+    CMP #$53
+    BNE Return
 
     ; check if moving
-	LDA !B6,x
-	BEQ Return
+    LDA !B6,x
+    BEQ Return
 
     ; check sprite state
-	LDA !14C8,x
-	CMP #$0A
-	BEQ SpriteBump
-	CMP #$09
-	BEQ SpriteBump
-	RTL
+    LDA !14C8,x
+    CMP #$0A
+    BEQ SpriteBump
+    CMP #$09
+    BEQ SpriteBump
+    RTL
 
 SpriteBump:
-	LDA !B6,x
-	BMI .going_left
+    LDA !B6,x
+    BMI .going_left
 
 .going_right
-	LDA #$D0 : STA !B6,x
-	BRA +
+    LDA #$D0 : STA !B6,x
+    BRA +
 
 .going_left
-	LDA #$30 : STA !B6,x
+    LDA #$30 : STA !B6,x
 +
     ; put in kicked state
-	LDA #$0A : STA !14C8,x
+    LDA #$0A : STA !14C8,x
 
     ; make block pass through
 	LDY #$00
 	LDA #$25
 	STA $1693|!addr
 
+if !PlaySFX
     ; Play bounce sfx
     LDA #$08
-	STA $1DFC|!addr
-Return:
-	RTL
+    STA $1DFC|!addr
+endif
 
-print "A block that bumps sprites, and nudges Mario."
+Return:
+    RTL
+
+print "A block that bounces throw blocks, and nudges Mario."
